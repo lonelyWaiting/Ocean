a30836b513e7ee20e899d28287cc8c01dad57771
lonelywaiting <lonelywaitinggm@gmail.com>
2018-09-01 15:42:05 +0800

    create uniform grid

--- Assets/Water.cs
+++ Assets/Water.cs
@@ -7,9 +7,52 @@ public class Water : MonoBehaviour {
     public Camera displacementCamera;
     private Material OceanMat;
 
-    Mesh CreateUniformGrid()
+    Mesh CreateUniformGrid(int resolutionX, int resolutionY, int width, int height)
     {
+        int vertNumX = resolutionX + 1;
+        int vertNumY = resolutionY + 1;
+
+        float dx = width / resolutionX;
+        float dy = height / resolutionY;
+
+        float du = 1.0f / resolutionX;
+        float dv = 1.0f / resolutionY;
+
+        int areaSize = vertNumX * vertNumY;
+
+        Vector3[] vertices  = new Vector3[areaSize];
+        Vector2[] texcoords = new Vector2[areaSize];
+
+        for(int y = 0; y < vertNumY; y++)
+        {
+            for(int x = 0; x < vertNumX; x++)
+            {
+                int index = y * vertNumX + x;
+                vertices[index]  = new Vector3(x * dx, 0.0f, y * dy);
+                texcoords[index] = new Vector2(x * du, y * dv);
+            }
+        }
+
+        int[] indices = new int[resolutionX * resolutionY * 6];
+        for(int y = 0, index = 0; y < resolutionY; y++)
+        {
+            for(int x = 0; x < resolutionX; x++)
+            {
+                indices[index++] = x + y * vertNumX;
+                indices[index++] = x + (y + 1) * vertNumX;
+                indices[index++] = (x + 1) + y * vertNumX;
+
+                indices[index++] = x + (y + 1) * vertNumX;
+                indices[index++] = (x + 1) + (y + 1) * vertNumX;
+                indices[index++] = x + 1 + y * vertNumX;
+            }
+        }
+
         Mesh mesh = new Mesh();
+        mesh.vertices  = vertices;
+        mesh.uv        = texcoords;
+        mesh.triangles = indices;
+
         return mesh;
     }
 
